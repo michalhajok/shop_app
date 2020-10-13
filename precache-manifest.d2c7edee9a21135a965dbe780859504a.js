c70aea3c563bc7f134029ab78b872af57291b079
Michał Hajok <hajokmichal@gmail.com>
2020-10-13 21:57:31 +0200

    Updates

---
+++ precache-manifest.d2c7edee9a21135a965dbe780859504a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1347a1502c4d7123f8e28503714a44af",
+    "url": "/shopApp/index.html"
+  },
+  {
+    "revision": "c1f075c86eb38a58e28c",
+    "url": "/shopApp/static/css/2.d22cd3da.chunk.css"
+  },
+  {
+    "revision": "ca624a4929f5ff4d4ba5",
+    "url": "/shopApp/static/css/main.c59f673b.chunk.css"
+  },
+  {
+    "revision": "c1f075c86eb38a58e28c",
+    "url": "/shopApp/static/js/2.d48942f4.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/shopApp/static/js/2.d48942f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca624a4929f5ff4d4ba5",
+    "url": "/shopApp/static/js/main.ecbeffa1.chunk.js"
+  },
+  {
+    "revision": "9ec3fa226da6f161afff",
+    "url": "/shopApp/static/js/runtime-main.a645c980.js"
+  }
+]);
